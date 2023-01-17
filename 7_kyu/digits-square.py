2dd85b4698020babbcabddc6cc462fc85d1e4cc5
Let'sRock <Dasha-Kuzmich-shf@yandex.ru>
2023-01-17 19:02:37 +0300

    Added 7_kyu 'Digit*Digit' kata solution

---
+++ 7_kyu/digits-square.py
@@ -0,0 +1,14 @@
+# DESCRIPTION:
+# Welcome. In this kata, you are asked to square every digit of a number
+# and concatenate them.
+#
+# For example, if we run 9119 through the function, 811181 will come out,
+# because 9^2 is 81 and 1^2 is 1. (81-1-1-81)
+#
+# Example #2: An input of 765 will/should return 493625 because 7^2 is 49,
+# 6^2 is 36, and 5^2 is 25. (49-36-35)
+#
+# Note: The function accepts an integer and returns an integer.
+
+def square_digits(num: int) -> int:
+    return int(''.join([str(int(sym)**2) for sym in str(num)]))
